576cb96085cbddb754d78c9f680942960557bdc1
vicksyy <victoriamarting7@gmail.com>
2025-11-24 12:49:47 +0100

    nombre FProject

--- compartir-proyectos/components/app-sidebar.tsx
+++ compartir-proyectos/components/app-sidebar.tsx
@@ -164,7 +164,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                   <Command className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">FPHub</span>
+                  <span className="truncate font-medium">FProject</span>
                   <span className="truncate text-xs">Portal</span>
                 </div>
               </a>
